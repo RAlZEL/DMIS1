0dddb8851185ae2d5148893a413c48b80359dc9b
John Paul Cudal <128292775+RAlZEL@users.noreply.github.com>
2025-09-10 16:42:09 +0800

    headachesolver

---
+++ app/Http/Livewire/User/InventoryManagement/Report/OfficeArticle/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\InventoryManagement\Report\OfficeArticle;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.inventory-management.report.office-article.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,9 @@
 <?php
-use App\Livewire\User\InventoryManagement\Report\OfficeArticle\Index;
-use Illuminate\Support\Facades\Route;
-
-Route::middleware(['auth'])->group(function () {
-	Route::get('/user/inventory-management-system/report/office-article', Index::class)
-		->name('user.inventory.report.office-article');
-});
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/',[MenuController::class,'userindex']);
-use App\Livewire\User\InventoryManagement\Report\OfficeArticle\Index;
-
-Route::middleware(['auth'])->group(function () {
-	Route::get('/user/inventory-management-system/report/office-article', Index::class)
-		->name('user.inventory.report.office-article');
-});
-<?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MenuController;
+use App\Http\Livewire\User\InventoryManagement\Report\OfficeArticle\Index as OfficeArticleReport;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('back.pages.auth.login');
-// });
-
-Route::get('/',[MenuController::class,'userindex']);
-// Route::get('/', ['middleware' =>'guest', function(){
-//     return view('auth.login');
-//   }]);
-// Route::get('/',[MenuController::class,'userindex']);
-
-// Route::get('/login', function () {
-//     return view('auth.login');
-// });
-
-
-
-
-
+Route::middleware(['auth'])->group(function () {
+    Route::get('/user/inventory-management-system/report/office-article', OfficeArticleReport::class)
+        ->name('user.inventory.report.office-article');
+});
